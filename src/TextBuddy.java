9398dabc5e7a55222f0fa0187b38244a62082366
Gerald <ngjingsiang@gmail.com>
2015-08-26 21:41:48 +0800

    Add 'add' command
    will get text for add
    text will be appended to file

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -1,9 +1,8 @@
 import java.io.File;
-
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
-
+import java.security.KeyStore.PrivateKeyEntry;
 import java.util.Scanner;
 
 public class TextBuddy {
@@ -12,20 +11,21 @@ public class TextBuddy {
 	private static final String FILE_LOCATION = "../";
 
 	private static final String MESSAGE_ASK_INPUT = "Command:";
+	private static final String MESSAGE_INVALID_COMMAND = "Invalid command";
+	
+	private static final String FORMAT_NEW_LINE = "\n";
 
 	// Possible commands
-	enum COMMANDS {
-		ADD, DELETE, DISPLAY, CLEAR;
-	}
+	private static final String[] COMMANDS = {"add", "delete", "display", "clear"};
+	
+	private static BufferedWriter out;
 	
 	public static void main(String[] fileName) {
 		try {
-			BufferedWriter out = getWriter(fileName);
+			out = getWriter(fileName);
 			String input = getInput();
 			String cmd = getCommand(input);
-			
-			System.out.println(cmd);
-			out.write(input);
+			executeCommand(cmd, getParam(input));
 			
 			out.close();
 		} catch (IOException e) {
@@ -55,8 +55,45 @@ public class TextBuddy {
 	// Read first word as command keyword
 	private static String getFirstWord(String input) {
 		// split at white space
-		return input.trim().split("\\s")[0];
+		String delimiter = "\\s";
+		return input.trim().split(delimiter)[0];
 	}
 	
+	// Perform action based on command given
+	private static Boolean executeCommand(String cmd, String text) {
+		Boolean flag = false;
+		String msg = "some success message";
+		
+		if (cmd.equalsIgnoreCase(COMMANDS[0])) {        // add
+			appendToFile(text);
+			flag = true;
+		} else if (cmd.equalsIgnoreCase(COMMANDS[1])) { // delete
+			System.out.println("delete");
+		} else if (cmd.equalsIgnoreCase(COMMANDS[2])) { // display
+			System.out.println("display");
+		} else if (cmd.equalsIgnoreCase(COMMANDS[3])) { // clear
+			System.out.println("clear");
+		} else {
+			
+		}
+		
+		System.out.println(msg);
+		System.out.println();
+		return flag;
+	}
+
+	private static String getParam(String input) {
+		int start = input.indexOf(" ");
+		return input.substring(start + 1);
+	}
+	
+	private static void appendToFile(String input) {
+		try {
+			out.write(input + FORMAT_NEW_LINE);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+ 	
 	
 }
