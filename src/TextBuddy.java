26953e1c32328d6f2d8e3e31525d92b4c1996997
Gerald <ngjingsiang@gmail.com>
2015-08-26 16:54:42 +0800

    Refactor getInput, Change tracking to .java instead of .class

---
+++ src/TextBuddy.java
@@ -0,0 +1,49 @@
+import java.io.File;
+
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class TextBuddy {
+	
+	private static final String FILE_FORMAT = ".txt";
+	private static final String FILE_LOCATION = "../";
+
+	private static final String MESSAGE_ASK_INPUT = "Command:";
+
+	// Possible commands
+	enum COMMANDS {
+		ADD, DELETE, DISPLAY, CLEAR;
+	}
+	
+	public static void main(String[] fileName) {
+		try {
+			BufferedWriter out = getWriter(fileName);
+			String input = getInput();
+			out.write(input);
+			
+			out.close();
+		} catch (IOException e) {
+			// TODO: handle exception. print exception for now
+			System.out.println(e);
+		}
+	}
+
+	private static String getInput() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(MESSAGE_ASK_INPUT);
+		String input = scanner.nextLine();
+		scanner.close();
+		
+		return input;
+	}
+
+	private static BufferedWriter getWriter(String[] fileName) throws IOException {
+		File file = new File(FILE_LOCATION + fileName[0] + FILE_FORMAT);
+		return new BufferedWriter(new FileWriter(file));
+	}
+	
+	
+}
